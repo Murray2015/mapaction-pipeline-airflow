d817609dcc4f458bbaaacc33f2b7d5aa67a5a814
Murray Hoggett <murray@truecircle.ai>
2024-04-09 06:47:42 +0100

    Added railroads download step. Not yet working.

--- dags/pipline_lib/utils.py
+++ dags/pipline_lib/utils.py
@@ -1,11 +1,14 @@
 import io
 import json
+from logging import getLogger
 import os
 import zipfile
 
 import requests
 import shapefile
 
+
+logger = getLogger(__name__)
 
 def add_string_to_filename(filename: str, string_to_add: str) -> str:
     """Adds a string to the end of a filename string before the extension."""
@@ -36,8 +39,10 @@ def shepefile_to_geojson(shapefile_path: str, geojson_path: str) -> None:
 def make_dir_download_zip(download_url: str, download_location: str,
                           output_file_location: str):
     """ Helper to download and extract a zip file. """
+    logger.info(f"Downloading data to {download_location}")
     os.makedirs(download_location, exist_ok=True)
     response = requests.get(download_url)
     z = zipfile.ZipFile(io.BytesIO(response.content))
     z.extractall(output_file_location)
     print("////", os.listdir(output_file_location))
+    logger.info(f"Download to {download_location} complete.")
